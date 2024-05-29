2723a9f29bea27f0a2afa04e61ec746fef539dbd
LeV1S268 <159754046+LeV1S268@users.noreply.github.com>
2024-05-30 02:57:38 +0300

    Update test1.cpp

--- test1.cpp
+++ test1.cpp
@@ -12,8 +12,3 @@ TEST(ChatRoomTest, SendMessageTest) {
 
     EXPECT_EQ(output, "[TestUser][TestRoom] Hello, World!\n");
 }
-
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
