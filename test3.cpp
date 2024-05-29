47ece7ee370ca3a4beadb75647a2a64d3f25148a
LeV1S268 <159754046+LeV1S268@users.noreply.github.com>
2024-05-30 01:25:47 +0300

    Update test3.cpp

--- test3.cpp
+++ test3.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include "chat_room.cpp"
+
 TEST(ChatRoomTest, MultipleChatRoomsTest) {
     ChatRoom chatRoom1("Room1");
     ChatRoom chatRoom2("Room2");
@@ -13,4 +14,9 @@ TEST(ChatRoomTest, MultipleChatRoomsTest) {
     std::string output = testing::internal::GetCapturedStdout();
 
     EXPECT_EQ(output, "[TestUser][Room1] Message 1\n[TestUser][Room2] Message 2\n");
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
